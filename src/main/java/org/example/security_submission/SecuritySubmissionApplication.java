50cb411c8d41fa798056706c19480866dd4d57cd
David Obrant <david.obrant@gmail.com>
2024-05-05 21:04:41 +0200

    initial

---
+++ src/main/java/org/example/security_submission/SecuritySubmissionApplication.java
@@ -0,0 +1,13 @@
+package org.example.security_submission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySubmissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySubmissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/security_submission/SecuritySubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.security_submission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
